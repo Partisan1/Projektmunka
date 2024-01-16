5f11c80a4212739c800c89831031529e28fd1d76
Partisan1 <benedekbarnabas.zsolt@gmail.com>
2024-01-16 20:27:32 +0100

    commentelés

--- kigyo.py
+++ kigyo.py
@@ -150,7 +150,7 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
-window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+window.geometry(f"{window_width}x{window_height}+{x}+{y}") #nem lehet float érték, csak int. ezért castoltam
 
 window.bind('<Left>', lambda event: change_direction('left'))
 window.bind('<Right>', lambda event: change_direction('right'))
